d8649a5f3fcfd91228b2c1b56b3f17d53fd0f0e0
Nghia <nphan@fandm.edu>
2020-05-10 19:36:17 -0500

    enforce eslint

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es6": true,
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:react/recommended"
+	],
+	"parserOptions": {
+		"ecmaFeatures": {
+			"jsx": true
+		},
+		"ecmaVersion": 2018,
+		"sourceType": "module"
+	},
+	"plugins": [
+		"react",
+	],
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"single"
+		],
+		"semi": [
+			"error",
+			"never"
+		],
+		"eqeqeq": "error",
+		"no-trailing-spaces": "error",
+		"object-curly-spacing": [
+			"error", "always"
+		],
+		"arrow-spacing": [
+			"error", { "before": true, "after": true }
+		],
+		"no-console": 0,
+		"react/prop-types": 0
+	}
+}
