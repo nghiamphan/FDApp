1635c7924ced5ce18a696d0c476c99a49e2deaad
Nghia <nphan@fandm.edu>
2020-05-17 19:10:44 -0500

    update search function

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,26 +1,32 @@
-const search = (query, state, includeComments) => {
-	query = query.toLowerCase()
-	const subs = Object.keys(state)
+const search = (filter, data, includeComments) => {
+	const query = filter.ticker
+		? filter.ticker.toLowerCase()
+		: ''
+	const subs = Object.keys(data)
 	let matches = []
 
 	for (let i = 0; i < subs.length; i++) {
-		const subreddit = state[subs[i]]
-		for (let j = 0; j < subreddit.data.length; j++) {
-			if (subreddit.data[j].title.toLowerCase().match(query)
-			|| subreddit.data[j].content.toLowerCase().match(query)) {
-				if (includeComments) {
-					const filteredComments = subreddit.data[j].comments.filter(comment =>
-						comment.content.toLowerCase().match(query)
-					)
-					matches.push({
-						...subreddit.data[j],
-						comments: filteredComments,
-					})
-				} else {
-					matches.push({
-						...subreddit.data[j],
-						comments: [],
-					})
+		const subreddit = data[subs[i]]
+		if (filter.subreddits.includes(subs[i])) {
+			for (let j = 0; j < subreddit.data.length; j++) {
+				if (subreddit.data[j].title.toLowerCase().match(query)
+				|| subreddit.data[j].content.toLowerCase().match(query)) {
+					if (includeComments) {
+						const filteredComments = subreddit.data[j].comments.filter(comment =>
+							comment.content.toLowerCase().match(query)
+						)
+						matches.push({
+							...subreddit.data[j],
+							comments: filteredComments,
+							subreddit: subs[i],
+						})
+					} else {
+						matches.push({
+							...subreddit.data[j],
+							comments: [],
+							subreddit: subs[i],
+						})
+					}
 				}
 			}
 		}
