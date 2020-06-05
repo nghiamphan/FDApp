e81d2bc3cccaad0273329ebabfb157759b31a74b
Nghia <nphan@fandm.edu>
2020-06-05 18:56:43 -0500

    implement a suggestion dropdown for the ticker search input

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,12 +4,38 @@ import { useDispatch, useSelector } from 'react-redux'
 import { fetchData } from '../reducers/dataReducer'
 import { updateFilter } from '../reducers/filterReducer'
 import { subreddits, wsb } from '../utils/constants'
+import companies from '../utils/tickers.json'
 
 const SearchForm = () => {
-	const { register, handleSubmit, } = useForm()
+	const { register, handleSubmit, watch, } = useForm()
+	let searchItem = watch('ticker')
 
 	const dispatch = useDispatch()
 	const flairs = useSelector(state => state.data[wsb].flairs)
+
+	// Search the searchItem against the list of companies' symbols and names, and return up to the best six matches
+	const filter = () => {
+		let matches = []
+		let goodMatches = 0
+
+		if (searchItem) {
+			searchItem = searchItem.toUpperCase()
+			for (let i = 0; i < companies.length; i++) {
+				const point = companies[i].symbol.indexOf(searchItem)
+				if (point >= 0) {
+					matches.push({ point: point, company: companies[i] })
+					if (point === 0)
+						goodMatches++
+				} else if (companies[i].name.toUpperCase().includes(searchItem)) {
+					matches.push({ point: 10, company: companies[i] })
+				}
+				if (goodMatches >= 6)
+					break
+			}
+		}
+		matches.sort((x, y) => x.point - y.point)
+		return matches.slice(0, 6)
+	}
 
 	const onSubmit = input => {
 		let subredditsToFetch = []
@@ -30,10 +56,21 @@ const SearchForm = () => {
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<label>Ticker</label>
 				<input
+					list="companies"
 					type="text"
+					autoComplete="off"
 					name="ticker"
 					ref={register()}
 				/>
+				<datalist id="companies">
+					{filter().map(match => {
+						return(
+							<option key={match.company.symbol} value={match.company.symbol}>
+								{match.company.name}
+							</option>
+						)})
+					}
+				</datalist>
 				<br/>
 				<label>Subreddits</label>
 				{subreddits.map((subreddit, index) => (
