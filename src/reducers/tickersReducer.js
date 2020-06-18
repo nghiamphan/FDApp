ea29c028c923e99f8b3ab7a8b930e5a65ff18ee3
Nghia <nphan@fandm.edu>
2020-06-17 23:08:32 -0500

    semantic: change variable names in tickersReducer.js

--- src/reducers/tickersReducer.js
+++ src/reducers/tickersReducer.js
@@ -1,4 +1,4 @@
-import companies from '../utils/tickers.json'
+import staticCompanies from '../utils/tickers.json'
 import { fetchTickers } from '../services/tickersFetch'
 
 const UPDATE_TICKERS = 'UPDATE_TICKERS'
@@ -8,10 +8,10 @@ const ONE_DAY = 86400000
 ////////////////////////
 // Reducer
 ////////////////////////
-const tickersReducer = (state = companies, action) => {
+const tickersReducer = (state = staticCompanies, action) => {
 	switch (action.type) {
 	case UPDATE_TICKERS:
-		return action.data ? action.data : state
+		return action.companies ? action.companies : state
 	default:
 		return state
 	}
@@ -20,18 +20,18 @@ const tickersReducer = (state = companies, action) => {
 export const updateTickers = () => {
 	return async dispatch => {
 		const local = JSON.parse(window.localStorage.getItem(KEY))
-		const fetchedCompanies = (local && local.expiry >= new Date().getTime())
+		const companies = (local && local.expiry >= new Date().getTime())
 			? local.companies
 			: await fetchTickers()
 
 		window.localStorage.setItem(KEY, JSON.stringify({
-			companies: fetchedCompanies,
-			expiry: new Date().getTime() + ONE_DAY
+			companies: companies,
+			expiry: new Date().getTime() + ONE_DAY,
 		}))
 
 		dispatch({
 			type: UPDATE_TICKERS,
-			data: fetchedCompanies,
+			companies,
 		})
 	}
 }
