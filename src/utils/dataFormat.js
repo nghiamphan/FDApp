b2e47c5943f716308cd2afa1e6f343cf2d91b434
Nghia <nphan@fandm.edu>
2020-06-21 15:44:59 -0500

    update displayLargeNumber helper function

--- src/utils/dataFormat.js
+++ src/utils/dataFormat.js
@@ -5,8 +5,12 @@ export const displayDate = seconds => {
 }
 
 export const displayLargeNumber = n => {
-	if (n >= 1000000) {
-		return (n/1000000).toFixed(2) + 'M'
+	if (n >= 1E12) {
+		return (n/1E12).toFixed(2) + 'T'
+	} else if (n >= 1E9) {
+		return (n/1E9).toFixed(2) + 'B'
+	} else if (n >= 1E6) {
+		return (n/1E6).toFixed(2) + 'M'
 	} else if (n >= 1000) {
 		return (n/1000).toFixed(2) + 'K'
 	} else
